e93accaa9f10fd2ed213fe325dec4faf3bf4e23a
Tomo Suzuki <suztomo@google.com>
2019-12-03 15:26:17 -0500

    Initial commit

---
+++ src/test/java/suztomo/AppTest.java
@@ -0,0 +1,18 @@
+package suztomo;
+
+import java.lang.reflect.Field;
+import org.apache.beam.sdk.schemas.FieldValueTypeInformation;
+import org.junit.Test;
+
+public class AppTest
+{
+    String f = "hello";
+
+    @Test
+    public void shouldExtractFieldValueInformation()
+    {
+        Field field = AppTest.class.getDeclaredFields()[0];
+        // This fails
+        FieldValueTypeInformation.forField(field);
+    }
+}
